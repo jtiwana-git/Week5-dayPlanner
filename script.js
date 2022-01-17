ff931623d7b93e982602ba56285fcd3d3e702586
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-01-17 19:22:14 +0000

    updated code and updated olanner

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("test");
+let day = moment();
+
+// Current day
+
+// Date added
+$("#currentDay").text(day.format("dddd Do MMMM YYYY"));
+
+// code for past block
+
+// code for current block
+
+// code for booked slot
