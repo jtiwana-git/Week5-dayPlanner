731d1fba4ef38d1b66d11d83364986d9bbbaaa9a
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-01-22 07:23:22 +0000

    deleted all all but time block, added code for LS

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 console.log("test");
 let day = moment();
+let time = moment().format('h');
+let pastHour = moment().subtract(1, "H").format("H");
+let aheadHour = moment().add(12, "H").format("H"); 
 
-// Current day
-
-// Date added
 $("#currentDay").text(day.format("dddd Do MMMM YYYY"));
 
-// code for past block
 
-// code for current block
 
-// code for booked slot
+
+let saveToPlanner = () => {
+    const textEl = document.querySelectorAll("textarea")
+const inputTextEl =document.querySelectorAll("inputText");
+    localStorage.setItem("Appointment9", textEl.text);
+  }
+
+  document.addEventListener("text", ()=>{
+      document.getElementById("save9").addEventListener('click', saveToPlanner);
+  });
