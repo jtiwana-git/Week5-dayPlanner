3015d731a5d1eedfa587eacf80a9f6a5917dcd6d
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-01-22 18:22:22 +0000

    update code to Jquery

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-console.log("test");
-let day = moment();
-let time = moment().format('h');
-let pastHour = moment().subtract(1, "H").format("H");
-let aheadHour = moment().add(12, "H").format("H"); 
+let today = moment().format("dddd Do MMMM YYYY");
+// Time of day on html
+$("#currentDay").text(today);
 
+// Time (Hour only)
+let hour = moment().format("HH");
 
-$("#currentDay").text(day.format("dddd Do MMMM YYYY"));
-
-$("time-block").text(day.format("hh"));
-
-
-$(document).ready(function() {
-    var block = $("row");
-
-$.each(time-block, function(block) {
-    $(time-block).removeClass("past");
-    $(time-block).removeClass("present");
-    $(time-block).removeClass("future");
-
-    // Turn "data-time" to number
-    var dataAttNum = $(time-block).data("time");
-    var dataTime = Number(dataAttNum);
-
-    if (dataTime < time) {
-        $(textarea).addClass("past");
-      } else if (dataTime === time) {
-        $(textarea).addClass("present");
-      } else if (dataTime > time) {
-        $(time-block).addClass("future");
-      }
-});
-
-});
-
-
-
-
-
-let saveToPlanner = () => {
+// Save button on click, save each appointments when booked
+$(".saveBtn").on("click", function (event) {
     const textEl = document.querySelectorAll("textarea")
     localStorage.setItem("Appointment9", textEl.text);
-  }
 
-  document.addEventListener("text", function(){
-      document.getElementById("save9").addEventListener('click', saveToPlanner);
-  });
+})
+
+// will change colour (block) depending on the time
+$("div").each(function () {
+    let workTime = parseInt($(this).attr("id"));
+    if (workTime > hour) {
+        $(this).addClass("future")
+
+    } else if (workTime == hour) {
+       $(this).addClass("present")
+    }
+    else {
+        $(this).addClass("past")
+
+    };
+})
+
+
+
