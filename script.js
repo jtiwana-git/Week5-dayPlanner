4be1f132359f9ce79f194e287eae29e9e4f37645
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-02-14 08:01:07 +0000

    updated code

--- script.js
+++ script.js
@@ -2,21 +2,24 @@ let today = moment().format("dddd Do MMMM YYYY");
 // Time of day on html
 $("#currentDay").text(today);
 
-// Time (Hour only)
+// Time - will display (Hour only)
 let hour = moment().format("HH");
+console.log(hour);
 
 
 // Save button on click, save each appointments when booked
 $("saveBtn").on("click", function (event) {
-    const textEl = document.getElementsByClassName("col-md-10 description");
-    localStorage.setItem("Appointment9", textEl.text);
+    const textEl = document.getElementById("09");
+
+$("09").localStorage.setItem("Appointment9", JSON.stringify(textEl.value));
  
 
 })
 
 // will change colour (block) depending on the time
 $("div").each(function () {
-    let workTime = parseInt($(this).attr("id"));
+    let workTime = parseInt($(this).attr("data-time"));
+    console.log (workTime);
     if (workTime > hour) {
         $(this).addClass("future")
 
